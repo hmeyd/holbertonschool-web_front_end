d6c0f7600803404f18af281980fa0dd6b01917c2
hmeyd <yyahmedsalm@example.com>
2025-03-27 01:20:13 +0100

    projet java

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,11 @@
+function taskFirst() {
+    var task = 'I prefer const when I can.';
+    return task;
+  }
+  
+function taskNext() {
+    var combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
